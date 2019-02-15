241e747c60019f1a4e59d330298c1dc492fe457c
Waleed Alfouzan <rpcgen@gmail.com>
2019-02-15 09:51:39 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace MicroservicesAsp.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+			var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+			var host = new WebHostBuilder().UseKestrel().UseStartup<Startup>().UseConfiguration(config).Build();
+			host.Run();
+        }
+		
+		
+		
+		
+    }
+}
